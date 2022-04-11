af190dedb0d9491f7ed240f9418140034da61e06
Jun Oh Lee <junohlee@Juns-MacBook-Pro.local>
2022-04-11 17:02:13 +0900

    algorithm

--- str/#20.Valid Parentheses.py
+++ str/#20.Valid Parentheses.py
@@ -1,18 +1,21 @@
 def isValid(s):
-    stack  = []
-    pairs = {"(" : ")", "[" : "]", "{" : "}"}
-    for i in range(len(s)):
-        parenthese = s[i]
+    stack = []
+    pairs = {"(" : ")", "[" : "]", "{":"}"}
+    for idx in range(len(s)):
+        parenthese = s[idx]
         if parenthese in pairs.keys():
             stack.append(parenthese)
         else:
             if stack == []:
-                return False 
+                return False
             else:
                 pop = stack.pop()
                 if parenthese != pairs[pop]:
                     return False
-                       
+    if stack == []:
+        return True
+
+
     if stack == []:
         return True    
 s1 = "()"
