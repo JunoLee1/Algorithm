8644be8cf2f9e759da4219a9ffabe1d63b89088d
Jun Oh Lee <junohlee@Juns-MacBook-Pro.local>
2022-04-12 22:36:32 +0900

    algorithm

--- str/#20.Valid Parentheses.py
+++ str/#20.Valid Parentheses.py
@@ -2,21 +2,17 @@ def isValid(s):
     stack = []
     pairs = {"(" : ")", "[" : "]", "{":"}"}
     for idx in range(len(s)):
-        parenthese = s[idx]
-        if parenthese in pairs.keys():
-            stack.append(parenthese)
+        parenthese = s[idx] 
+        if parenthese in pairs.keys(): #만약 오른쪽 짝이 키값에 있다면
+            stack.append(parenthese) #stack값에 추가하라
         else:
-            if stack == []:
-                return False
+            if stack == []: #오른쪽 키값에 없으면 
+                return False # 거짓
             else:
-                pop = stack.pop()
+                pop = stack.pop() # 오른쪽 괄호와 stack의 마지막 값을 제외한 나머지의 짝이 다를경우 
                 if parenthese != pairs[pop]:
                     return False
     if stack == []:
-        return True
-
-
-    if stack == []:
-        return True    
+        return True  
 s1 = "()"
 print(isValid(s1))
