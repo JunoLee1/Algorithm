84ac3d23070503d6c6b6edc5cef501f4b3349660
Jun Oh Lee <junohlee@Juns-MacBook-Pro.local>
2022-05-04 19:55:15 +0900

    algorithm

---
+++ Array/.py
@@ -0,0 +1,14 @@
+def tribonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    dp = [0]*(n+1)
+    dp[1] = 1
+    for i in range(3,n+1):
+        dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+    return dp[n]
+n = 3
+print(tribonacci(n))
