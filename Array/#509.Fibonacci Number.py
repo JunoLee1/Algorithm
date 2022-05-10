c108b3753f21e39b2db18e55bcf3956673f28ef0
Jun Oh Lee <junohlee@Juns-MacBook-Pro.local>
2022-05-10 18:02:00 +0900

    algorithm

--- Array/#509.Fibonacci Number.py
+++ Array/#509.Fibonacci Number.py
@@ -3,10 +3,7 @@ def Fib(n):
         return 0
     if n == 1:
         return 1
-    dp = [0]*(n+1)
-    dp[1] = 1
-    for i in range(2,n+1):
-        dp[i] = dp[i-1] + dp[i-2]
-    return dp[n]
+    else :
+        return Fib(n-1)+ Fib(n-2)
 n = 4
 print(Fib(n))
