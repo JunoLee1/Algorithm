cc884686183aa53a9a093873c891b663e93e9312
Jun Oh Lee <junohlee@Juns-MacBook-Pro.local>
2022-06-28 19:55:53 +0900

    algorithm

--- Array/#242.ValidAnagram.py
+++ Array/#242.ValidAnagram.py
@@ -1,7 +1,10 @@
+from collections import defaultdict
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dic1 = {}
-        dic2 = {}
+        dic1 = defaultdict(int)
+        dic2 = defaultdict(int)
         for item in s:
             dic1[item] = dic1.get(item,0) + 1
         for item in t:
