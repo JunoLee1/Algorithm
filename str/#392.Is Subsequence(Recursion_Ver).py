05586803c45270f234ea9480953776d7202396dc
Jun Oh Lee <junohlee@Juns-MacBook-Pro.local>
2022-06-29 23:39:49 +0900

    algorith

--- str/#392.Is Subsequence(Recursion_Ver).py
+++ str/#392.Is Subsequence(Recursion_Ver).py
@@ -8,6 +8,27 @@ def isSubsequence(self, s, t):
     else:
         return self.isSubsequence(s,t[1:])
 
-s1 = "abc"
-t1 = "ahbgdc"
-print(isSubsequence(s1,t1))
+def isSubsequence(self, s, t):
+    m = len(s)
+    n = len(t)
+
+    if n == 0: #len(s)가 0이면 true
+        return True
+        
+    if m == 0:
+        return False
+        
+    dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+                
+            if t[i-1] == s[j-1]: #t와 s의 뽑은 요소가 같다면
+                dp[i][j] = dp[i-1][j-1] + 1 #LCS(i-1,j-1)에다가 길이 +1
+                
+            else: 
+                dp[i][j] = max(dp[i][j-1],dp[i-1][j])
+            if dp[i][j] == len(s):
+                return True
+    return False
+        
