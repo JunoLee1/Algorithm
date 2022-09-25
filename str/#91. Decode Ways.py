089eec742f149a2547c5315b041af9f44bf694b7
Jun Oh Lee <junohlee@Juns-MacBook-Pro.local>
2022-09-25 17:33:44 +0900

    algorithm

--- str/#91. Decode Ways.py
+++ str/#91. Decode Ways.py
@@ -2,13 +2,16 @@ class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
         dp = [0] * (n+1)
-        split = s.split() #랜덤으로 s를 나눠주고
-        for i in range(n):
-            for as_num in split:
-                if len(split[int(as_num)]) <= 3:#숫자의 아스키 넘버를 먼저 구하고, 그숫자들에다가 +64를 더해 준다음 대문자 아스키 넘버를 구하고, 이 아스키 넘버로 대문자들의 갯수를 구해준다 
-                    temp[i] += str(int(split[int(as_num)]) + 64)
-                    dp[i] = temp[i]
-                else:
-                    if s[0] == '0':
-                        return 0
-            return len(dp)
+        dp[0] = 1 #to count each 
+        if not s:
+            return 0
+        
+        for i in range(1, n + 1):
+            
+            if s[i - 1] != "0":
+                dp[i] += dp[i - 1]
+            
+            if len(s[i-2 : i]) == 2 and "10" <= s[i-2 : i] <= "26":
+                dp[i] += dp[i - 2]
+        return dp[n]
+            
