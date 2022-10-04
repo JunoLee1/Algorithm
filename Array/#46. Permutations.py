14575f388cc42da97ecfc31547e3e1fc6f3a2fb3
Jun Oh Lee <junohlee@Juns-MacBook-Pro.local>
2022-10-04 15:29:50 +0900

    algorithm

---
+++ Array/#46. Permutations.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        self.helper(nums, [], ans)
+        return ans
+    
+    def helper(self, nums, temp, ans):
+        if len(nums) == 0:
+            ans.append(temp)
+        
+        for i in range(len(nums)):
+            self.helper(nums[:i] + nums[i + 1:], temp + [nums[i]], ans)
+            
+            
+            
+            
+#                                temp nums
+#                               [] [1, 2, 3]
+
+
+#            [1][2, 3]                      [2][1, 3]                         [3][1, 2]
+#     [1, 2][3]   [1, 3][2]           [2, 1][3]    [2, 3][1]           [3, 1][2]   [3, 2][1]
+# [1, 2, 3][]        [1, 3, 2][]   [2, 1, 3][]       [2, 3 ,1][]  [3, 1, 2][]        [3, 2, 1][]
+
+# len(nums) == 0
