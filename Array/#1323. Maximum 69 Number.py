78d42b9543d0d5414fe53aa04905529126dd7249
Jun Oh Lee <junohlee@Juns-MacBook-Pro.local>
2022-12-16 21:02:27 +0900

    algorithm

--- Array/#1323. Maximum 69 Number.py
+++ Array/#1323. Maximum 69 Number.py
@@ -8,8 +8,11 @@ class Solution:
             else:
                 uniq[i] = "6"
         for idx in uniq: 
-            if uniq[idx] == "9": # if there is "6" in num, changes the part to 9 one most digit
+            if uniq[idx] == "9": # check if each uniq val "9" or not, changes the part to 9 one most digit
                 return int(num[:idx] + uniq[idx] + num[idx+1:]) # changes one most number 
 
-        return int(num) #왜 한칸더 인덴테이션 더해야하나요??
-        
+            # if uniq val is not "9", keep find "9"
+
+            
+            
+        return int(num) # if there is no  "9" which is unique val in uniq, return origin number
