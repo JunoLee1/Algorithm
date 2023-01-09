2707881ae681a914bd012b9b53cbbd98acf8a440
Jun Oh Lee <junohlee@Juns-MacBook-Pro.local>
2023-01-09 20:14:31 +0900

    algorithm

---
+++ Linked List/#817. Linked List Components.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+        count = 0
+        node = head #traverse 
+        while node:
+            if node.val in nums:
+                count+=1
+                while node and node.val in nums:
+                    node = node.next
+            else :node = node.next
+        return count
