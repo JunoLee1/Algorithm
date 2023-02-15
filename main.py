64b84ad4b725e3bcb77e4e0b02c28d47466b4da6
Jun Oh Lee <junohlee@Juns-MacBook-Pro.local>
2023-02-15 20:36:23 +0900

    algorithm

---
+++ main.py
@@ -0,0 +1,27 @@
+# import another_module
+# print(another_module.another_variable)
+#from turtle import Turtle,Screen
+from prettytable import PrettyTable 
+table = PrettyTable()
+table.add_column('pokemon names',['Pikachu', 'Squirtle', 'Charmander'])
+table.add_column('types',['fire', 'water', 'grass'])
+table.align = "l"
+print(table.align)
+
+print(table)
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.position()
+# timmy.forward(100)
+# timmy.position()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+
+
+
---
+++ quiz game.py
@@ -0,0 +1,4 @@
+class Quertion:
+    def __init__(self,text,anwer):
+        self.text = text
+        self.answer = answer
